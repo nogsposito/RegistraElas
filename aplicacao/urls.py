68c0d656f41ac51ceadfbf23c528b023404ac2bd
nogsposito <vsan@cesar.school>
2024-10-03 18:41:35 -0300

    url principal agora é a home, agora rodando o dominio sem / nada já abre direto

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'aplicacao'
 
 urlpatterns = [
-    path('home/', views.CadastrosView.as_view(), name = "home"),
+    path('', views.CadastrosView.as_view(), name = "home"),
     path('visualizar/<int:id>/', views.CadastroDetalhadoView.as_view(), name='visualizar_cadastros'),
 ]
