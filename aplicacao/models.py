700e9ef5fe741d7aaf3984b266b40aa32fffc277
Pedro Caldas <pedro.caldas009@gmail.com>
2024-10-07 19:34:12 -0300

    banco de dados com rows novas

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
 class cadastros(models.Model):
-    nome = models.CharField(max_length=80)
-    idade = models.CharField(max_length=30)
-    comentarios = models.CharField(max_length=500, null=True)
+    nome = models.CharField(max_length=150)
+    idade = models.IntegerField(default=0)
+    cpf = models.CharField(max_length=11, default="")
+    celular = models.CharField(max_length=15, default="")
+    cep = models.CharField(max_length=8, default="")
+    cidade = models.CharField(max_length=30, default="Cidade não informada")
+    bairro = models.CharField(max_length=100, default="Bairro não informado")
+    rua = models.CharField(max_length=150, default="Rua não informada")
+    numero = models.IntegerField(default=0)
+    complemento = models.CharField(max_length=50, default="Complemento não informado")
